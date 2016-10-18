7724a1ba5e42e7e511071954d79e06323eebe78b
Markus Makela <markus.makela@mariadb.com>
2016-10-18 14:06:32 +0300

    Move service user injection into MySQL authenticator
    The MySQL authenticator now injects the service user into the list of
    allowed users if loading of database users fails. This allows the removal
    of common code in the binlogrouter and maxinfo modules.

--- server/modules/routing/binlog/blr.c
+++ server/modules/routing/binlog/blr.c
@@ -1790,7 +1790,7 @@ static void rses_end_locked_router_action(ROUTER_SLAVE *rses)
 
 static int getCapabilities()
 {
-    return (int)(RCAP_TYPE_NO_RSESSION | RCAP_TYPE_NO_USERS_INIT);
+    return RCAP_TYPE_NO_RSESSION;
 }
 
 /**
